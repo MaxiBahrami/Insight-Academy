fc4b060ba1115505c42cae521c2b4a8eaa544208
hafizfarhan <muhammad@commercegenie.com>
2024-03-27 15:54:34 +0500

    Blogs, SearchList and TutorDetails page are added in MVC

--- InsightAcademy/Controllers/HomeController.cs
+++ InsightAcademy/Controllers/HomeController.cs
@@ -23,6 +23,19 @@ namespace InsightAcademy.Controllers
             return View();
         }
 
+        public IActionResult TutorDetails()
+        {
+            return View();
+        }
+        public IActionResult Blogs()
+        {
+            return View();
+        }
+
+        public IActionResult SearchListing()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
