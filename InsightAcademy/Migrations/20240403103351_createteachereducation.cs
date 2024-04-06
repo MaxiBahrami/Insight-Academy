19d73fde8c45840078a15d82cbc0d57b56d2b1e5
Muhammad Farhan <hafizfarhan0039@gmail.com>
2024-04-06 15:54:35 +0500

    all change for captcha and profile and education

---
+++ InsightAcademy/Migrations/20240403103351_createteachereducation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InsightAcademy.Migrations
+{
+    /// <inheritdoc />
+    public partial class createteachereducation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TeacherEducation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProfileId = table.Column<int>(type: "int", nullable: false),
+                    EducationTile = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DayDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    University = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<int>(type: "int", nullable: true),
+                    ModificationDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedBy = table.Column<int>(type: "int", nullable: true),
+                    DeletionDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeacherEducation", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TeacherEducation_TeacherProfile_ProfileId",
+                        column: x => x.ProfileId,
+                        principalTable: "TeacherProfile",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeacherEducation_ProfileId",
+                table: "TeacherEducation",
+                column: "ProfileId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TeacherEducation");
+        }
+    }
+}
---
+++ InsightAcademy/Migrations/20240404073349_addcolumnzipcode.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InsightAcademy.Migrations
+{
+    /// <inheritdoc />
+    public partial class addcolumnzipcode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Zipcode",
+                table: "TeacherProfile",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Zipcode",
+                table: "TeacherProfile");
+        }
+    }
+}
